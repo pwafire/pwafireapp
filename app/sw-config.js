2fcf90d60c27d159f91bc7b60e7c323958785285
Maye Edwin <Edwin@impala.com>
2018-07-27 15:09:39 +0300

    add-images

--- app/sw-config.js
+++ app/sw-config.js
@@ -6,11 +6,17 @@ module.exports = {
     // eg cache all css file in the root folder
     "index.html",
     "images/*.png",
+    "images/*.svg",
+    "images/*.jpg",
+    "images/*.gif",
     "js/*.js",
-    "css/*.css"
+    "css/*.css",
+    "scss/*.scss"
     ],
-    "swSrc": "src/service-worker.js", // The path and filename of the service worker file that will be created by the build process.
-    "swDest": "build/service-worker.js", // The path to the source service worker file that can contain your own customized code,
+    // The path and filename of the service worker file that will be created by the build process.
+    "swSrc": "src/service-worker.js", 
+    // The path to the source service worker file that can contain your own customized code,
+    "swDest": "build/service-worker.js", 
     // in addition to containing a match for injectionPointRegexp.
    "globIgnores": [
     "../sw-config.js"
